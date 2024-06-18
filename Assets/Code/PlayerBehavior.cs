68d6eaf30c7d377813ede1bb2d15883e5c772c01
RyukiAs <shionscars@gmail.com>
2024-06-18 16:15:57 -0500

    update

---
+++ Assets/Code/PlayerBehavior.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    //never set the value of a public variable here
+    public float speed;
+    public GameObject bulletPrefab;
+
+    public float secondsBetweenShots;
+    float secondsSinceLastShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        secondsSinceLastShot = secondsBetweenShots;
+        References.thePlayer = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //WASD to move
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+        ourRigidBody.velocity = inputVector * speed;
+
+        Ray rayFromCameraToCursor = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        playerPlane.Raycast(rayFromCameraToCursor, out float distanceFromCamera);
+        Vector3 cursorPosition = rayFromCameraToCursor.GetPoint(distanceFromCamera);
+
+        //look at new position
+        Vector3 lookAtPosition = cursorPosition;
+        transform.LookAt(lookAtPosition);
+
+        //Firing
+        secondsSinceLastShot += Time.deltaTime;
+
+        //Click to fire
+        // if clicked, create a bullet at current position
+        if (Input.GetButton("Fire1") && secondsSinceLastShot >= secondsBetweenShots)
+        {
+            Instantiate(bulletPrefab, transform.position + transform.forward, transform.rotation);
+            //transform.position += Vector3.up * 0.01f;
+            secondsSinceLastShot = 0;
+        }
+
+    }
+}
