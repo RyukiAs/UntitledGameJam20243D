2f5fe56492f25943c433ebca38685af49a9a3a23
Dylan Oldham <dylansoldham@gmail.com>
2024-06-18 15:19:42 -0700

    Added arena generator

---
+++ Assets/Code/ArenaController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaController : MonoBehaviour
+{
+    public GameObject arenaSlicePrefab;
+
+    private int nextSliceIndex = 0;
+    void CreateNextSlice()
+    {
+        GameObject slice = Instantiate(arenaSlicePrefab);
+
+        Vector3 newScale = slice.transform.localScale;
+        newScale.x *= Mathf.Pow(5.0f, nextSliceIndex);
+        newScale.y *= Mathf.Pow(2.0f, nextSliceIndex);
+        newScale.z *= Mathf.Pow(5.0f, nextSliceIndex);
+        slice.transform.localScale = newScale;
+        nextSliceIndex++;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateNextSlice();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 3; i++) {
+            CreateNextSlice();
+        }
+    }
+}
