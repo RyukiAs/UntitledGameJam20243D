2cc0fe9a53a6bf65ef9add78e9852cb69c888f7a
Dylan Oldham <dylansoldham@gmail.com>
2024-06-20 14:03:47 -0700

    Added flexibility to arena controller

--- Assets/Code/ArenaController.cs
+++ Assets/Code/ArenaController.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class ArenaController : MonoBehaviour
 {
     public GameObject arenaSlicePrefab;
+    public GameObject arenaFloor;
+    public float heightGrowthFactor = 5.0f;
 
     private int nextSliceIndex = 0;
-    private float sliceGrowthFactor = 5.0f;
+    
     void CreateNextSlice()
     {
         GameObject slice = Instantiate(arenaSlicePrefab);
 
+
+        float wallWidth = slice.transform.GetChild(0).transform.GetChild(0).localScale.z;
+        float growthFactor = 1.0f / (1.0f - 2.0f * wallWidth);
+
         Vector3 newScale = slice.transform.localScale;
-        newScale.x *= Mathf.Pow(5.0f, nextSliceIndex);
-        newScale.y *= Mathf.Pow(2.0f, nextSliceIndex);
-        newScale.z *= Mathf.Pow(5.0f, nextSliceIndex);
+        newScale.x = arenaFloor.transform.localScale.x * Mathf.Pow(growthFactor, nextSliceIndex + 1);
+        newScale.y = Mathf.Pow(heightGrowthFactor, nextSliceIndex + 1);
+        newScale.z = arenaFloor.transform.localScale.z * Mathf.Pow(growthFactor, nextSliceIndex + 1);
         slice.transform.localScale = newScale;
+
+        slice.transform.position = new Vector3(slice.transform.position.x, newScale.y / 2.0f, slice.transform.position.z);
+
         nextSliceIndex++;
     }
 
