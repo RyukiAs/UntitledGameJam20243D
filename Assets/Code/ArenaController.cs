1bc7c2bcd41404f0256f09931764c28572fdd4a8
Dylan Oldham <dylansoldham@gmail.com>
2024-06-23 14:57:08 -0700

    Increased num slices

--- Assets/Code/ArenaController.cs
+++ Assets/Code/ArenaController.cs
@@ -32,7 +32,7 @@ public class ArenaController : MonoBehaviour
     void Start()
     {
         CreateNextSlice();
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 10; i++)
         {
             CreateNextSlice();
         }
